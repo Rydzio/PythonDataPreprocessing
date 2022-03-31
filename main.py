9b70ac3e44dcb1f93cb8d3833ad2a56f1b50852c
rytych <michal.rytych@geminibs.com>
2022-03-31 19:32:27 +0200

    Fill missing data

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ y = dataset.iloc[:, -1].values
 # Print the data
 print(x)
 print(y)
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Print new table
+print(x)
