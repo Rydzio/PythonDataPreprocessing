73152f6132663ed46e2d24157aafeddbff1e5772
rytych <michal.rytych@geminibs.com>
2022-04-27 22:04:38 +0200

    Transform array to numpy array (expected future input) and transform yes/no to binary representation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn as skl
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -14,10 +15,28 @@ print(y)
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
-
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # Print new table
+print("\n", x)
+
+# Encoding categorical data
+
+# Encoding the Independent Variable
+#from numpy.ma.core import remainder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+print()
 print(x)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print("\n", y)
